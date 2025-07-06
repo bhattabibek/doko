93c0d83cc09a1ccb9412d62346c404345de0db30
Bibek Bhatta <bhattabbk1@gmail.com>
2025-07-06 13:07:43 +0545

    feat:newsletter and footer UI added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Hero from '../components/Hero';
 import Category from '../components/Category';
 import BestSeller from '../components/BestSeller';
+import NewsLetter from "../components/NewsLetter";
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
 <Hero/>
 <Category/>
 <BestSeller/>
+<NewsLetter/>
+<Footer/>
   </>
   )
 }
